afc3f13958d6c08d884e09a4d9d25173591897dc
TonyChyi <tonychee1989@gmail.com>
2015-11-07 15:41:04 +0800

    pep8

--- automail.py
+++ automail.py
@@ -7,13 +7,14 @@ import time
 import re
 import email
 import smtplib
+import os
 
 yesterday = time.localtime(time.time() - 86400)
 today = time.localtime(time.time())
 
-##################################################################
+########################以下内容可修改#################################
 
-logfile = '#MidyMidymc_%s.log' % time.strftime('%Y%m%d', yesterday)
+logfile = os.environ['HOME'] + '#MidyMidymc_%s.log' % time.strftime('%Y%m%d', yesterday)
 filterfile = 'filter.txt'
 
 MASK = '***奇怪***'
@@ -22,12 +23,11 @@ ME = 'mc_bot <no-replay@MidyMidyMC>'
 TO = 'leo_song <leo_songwei@outlook.com>'
 CONTENT_PREFIX = '%s聊天记录' % time.strftime('%Y年%m月%d日', yesterday)
 
-##################################################################
+########################以上内容可修改#################################
 
-'''
-邮件正文生成
-'''
+
 def gen_content():
+    '''邮件正文生成'''
     mail = '%s\n\n' % CONTENT_PREFIX
     for line in open(logfile):
         if re.search(r'\*\*\*', line):
@@ -37,32 +37,34 @@ def gen_content():
         mail += filter(line)
     return mail
 
-'''
-奇怪的词语过滤
-'''
+
 def filter(msg):
+    '''奇怪的词语过滤'''
     for line in open('filter.txt', 'r'):
         if line != '':
             msg = re.sub(line.replace('\n', ''), MASK, msg)
 
     return msg
 
+
 def make_mail(payload):
+    '''生成邮件'''
     msg = email.Message.Message()
-    msg['To'] = TO 
-    msg['From'] = ME 
+    msg['To'] = TO
+    msg['From'] = ME
     msg['Date'] = time.strftime('%Y-%m-%d', today)
     msg['Subject'] = SUBJECT % time.strftime('%Y年%m月%d日', yesterday)
     msg['Content-Type'] = 'text/plain; charset=utf-8'
     msg['Content-Transfer-Encoding'] = '8bit'
     msg.set_payload(payload)
-    return msg 
+    return msg.as_string()
+
 
 def send_mail(mail):
-    #r = re.compile(r'<(.+@.+)>')
-    #from_ = r.findall(ME)[0]
-    #to = r.findall(TO)[0]
+    '''使用SMTP发送邮件'''
     S = smtplib.SMTP('localhost')
     S.send(mail)
+    S.quit()
 
-print send_mail(make_mail(gen_content()))
+if __name__ == '__main__':
+    send_mail(make_mail(gen_content()))
